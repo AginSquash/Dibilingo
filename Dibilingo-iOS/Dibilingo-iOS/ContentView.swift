aca41c7c887f622e55227ec792c01b263992d886
AginS <agins.main@gmail.com>
2020-09-18 17:32:49 +0300

    Dibilingo-iOS created

---
+++ Dibilingo-iOS/Dibilingo-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dibilingo-iOS
+//
+//  Created by Vlad Vrublevsky on 18.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dibilingo-iOS/Dibilingo-iOS/Dibilingo_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dibilingo_iOSApp.swift
+//  Dibilingo-iOS
+//
+//  Created by Vlad Vrublevsky on 18.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct Dibilingo_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
