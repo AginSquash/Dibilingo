79d94cf7366dea8a5b3bfa2e8833107b7f71e8f4
AginS <agins.main@gmail.com>
2020-12-05 15:44:10 +0300

    added custom tableview

--- Dibilingo-iOS/Dibilingo-iOS/Views/MainmenuView.swift
+++ Dibilingo-iOS/Dibilingo-iOS/Views/MainmenuView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct cutomIcon: View {
     var icon_name: String
     
+    /*
     var destination: some View {
         switch icon_name {
         case "icon_cat":
@@ -20,17 +21,70 @@ struct cutomIcon: View {
             return AnyView(Text("Level 3 here"))
         }
         //return Text("Level 3 here").navigationBarHidden(true)
-    }
+    } */
+    
+    @State private var showPopup: Bool = false
     
     var body: some View {
-        NavigationLink(
-            destination: destination,
-            label: {
-                Image(icon_name)
-                    .resizable()
-                    .shadow(radius: 5)
-                    .frame(width: 200, height: 200, alignment: .center)
-            })
+        VStack {
+            Image(icon_name)
+                .resizable()
+                .shadow(radius: 5)
+                .frame(width: 200, height: 200, alignment: .center)
+                .offset(y: showPopup ? 75 : 0 )
+                .onTapGesture(count: 1, perform: {
+                    withAnimation { showPopup.toggle() }
+                })
+            if showPopup {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 50)
+                        .frame(width: 275, height: 150, alignment: .center)
+                        .foregroundColor(.yellow)
+                    HStack {
+                        NavigationLink(
+                            destination: EmojiWordView().navigationBarHidden(true),
+                            label: {
+                                ZStack {
+                                RoundedRectangle(cornerRadius: 25)
+                                    .frame(width: 75, height: 100, alignment: .center)
+                                Text("1")
+                                    .font(Font.custom("Coiny", size: 42))
+                                    .foregroundColor(.white)
+                                }
+                            })
+                        //
+                        
+                        NavigationLink(
+                            destination: IrregVerbView(),
+                            label: {
+                                ZStack {
+                                RoundedRectangle(cornerRadius: 25)
+                                    .frame(width: 75, height: 100, alignment: .center)
+                                Text("2")
+                                    .font(Font.custom("Coiny", size: 42))
+                                    .foregroundColor(.white)
+                                }
+                            })
+                        //
+                        
+                        NavigationLink(
+                            destination: Text("3 lvl"),
+                            label: {
+                                ZStack {
+                                RoundedRectangle(cornerRadius: 25)
+                                    .frame(width: 75, height: 100, alignment: .center)
+                                Text("3")
+                                    .font(Font.custom("Coiny", size: 42))
+                                    .foregroundColor(.white)
+                                }
+                            })
+                        //
+                        
+                    }
+                }
+                .offset(y: 75)
+            }
+        }
     }
 }
 
@@ -46,9 +100,13 @@ struct MainmenuView: View {
                 ZStack {
                     ScrollView {
                         
+                        Rectangle()
+                            .frame(width: 100, height: 100, alignment: .center)
+                            .opacity(0)
+                        
                         ForEach(categories) { category in
                             cutomIcon(icon_name: category.name)
-                                .position(x: geo.frame(in: .global).midX + 70 * (category.id % 2 == 0 ? 1: -1) , y: 100)
+                                .position(x: geo.frame(in: .global).midX + 65 * (category.id % 2 == 0 ? 1: -1))
                         }
                     }
                 }
