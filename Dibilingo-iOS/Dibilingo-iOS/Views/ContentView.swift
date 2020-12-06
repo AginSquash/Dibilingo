f2bd791bc43594db9d9d6006bbc1cb56a76b9a73
AginS <agins.main@gmail.com>
2020-12-07 01:51:10 +0300

    commeted legacy code

--- Dibilingo-iOS/Dibilingo-iOS/Views/ContentView.swift
+++ Dibilingo-iOS/Dibilingo-iOS/Views/ContentView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-
+/*
 struct ContentView: View {
     
     var body: some View {
@@ -84,3 +84,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+*/
