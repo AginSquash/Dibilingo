a10760f44788e4e738eeac55225c83261f257881
AginS <agins.main@gmail.com>
2020-12-07 14:00:01 +0300

    added ssl files

--- Server/app.py
+++ Server/app.py
@@ -122,4 +122,4 @@ if __name__ == '__main__':
     updateCardList()
     dataHash = checksumdir.dirhash("data/")
     print("DataHash: {}".format(dataHash))
-    app.run(host="0.0.0.0", port="5000", debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True) #, ssl_context=('cert.pem', 'key.pem')
