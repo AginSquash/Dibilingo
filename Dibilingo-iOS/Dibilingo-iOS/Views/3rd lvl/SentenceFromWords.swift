16d50cdab89555b2d8f2e1ebf18475e52ece2e6a
AginS <agins.main@gmail.com>
2020-12-14 16:21:37 +0300

    rework ui

--- Dibilingo-iOS/Dibilingo-iOS/Views/3rd lvl/SentenceFromWords.swift
+++ Dibilingo-iOS/Dibilingo-iOS/Views/3rd lvl/SentenceFromWords.swift
@@ -11,67 +11,58 @@ struct SentenceFromWords: View {
     
     @State private var sentence: String = "AAA"
     @State private var words = [identifiable_word]()
-    @State private var rows = [
-        GridItem(.flexible()),
-        GridItem(.flexible()),
-        GridItem(.flexible())
-    ]
-    
-    // sorted words little-big-little
 
-    var words_paired: [identifiable_word] {
-        var words_sorted = words.sorted(by: { $0.text.count < $1.text.count })
-        
-        var words = [identifiable_word]()
-        
-        while words_sorted.count != 0 {
-            let w1 = words_sorted.removeFirst()
-            words.append(w1)
-            guard words_sorted.count > 0 else {
-                break
-            }
-            let w2 = words_sorted.removeLast()
-            words.append(w2)
-            
-            guard words_sorted.count > 0 else {
-                break
-            }
-            let w3 = words_sorted.removeFirst()
-            words.append(w3)
-        }
-        
-        
-        
-        return words //.shuffled()
-    }
-    
+    @State private var entered_sentence = [String]()
     var body: some View {
         ZStack {
+            
             VStack {
-                //Text(sentence)
+                Spacer()
+                Text(entered_sentence.combineToString())
+                    .font(Font.custom("boomboom", size: 20))
+                    .multilineTextAlignment(.leading)
                 
-                let columns = [
-                    GridItem(.flexible()),
-                    GridItem(.flexible()),
-                    GridItem(.flexible())
-                ]
-                LazyVGrid(columns: columns) {
+                if entered_sentence.count != 0 {
+                    Button(action: {
+                        
+                        withAnimation {
+                            words .append(contentsOf: entered_sentence.map( { identifiable_word($0) }) )
+                            entered_sentence.removeAll()
+                            
+                            print(words)
+                        }
+                        
+                    }, label: {
+                        Image(systemName: "x.circle")
+                            .resizable()
+                            .foregroundColor(.red)
+                            .scaledToFit()
+                            .frame(width: 30, height: 30, alignment: .center)
+                    })
+                    .transition(.scale)
+                }
                 
-                //LazyHGrid(rows: rows) {
-                    ForEach(words_paired) { word in
-                        Text(word.text)
-                    }
-                }.frame(height: 100, alignment: .center)
-                
+                Spacer()
             }
+            
+            VStack {
+                Spacer()
+                WordView(words: $words) { word in
+                    withAnimation {
+                        entered_sentence.append(word)
+                    }
+                }
+                .padding()
+            }
+            
         }
         .onAppear(perform: {
-            self.words.append(identifiable_word("long_woord"))
-            self.words.append(identifiable_word("word"))
-            self.words.append(identifiable_word("wordkdkkaka"))
-            self.words.append(identifiable_word("words"))
-            self.words.append(identifiable_word("a"))
-            self.words.append(identifiable_word("wordaapppa"))
+            words.append(identifiable_word("long_woord"))
+            words.append(identifiable_word("word"))
+            words.append(identifiable_word("wordkdkkaka"))
+            words.append(identifiable_word("words"))
+            words.append(identifiable_word("a"))
+            words.append(identifiable_word("wordaapppa"))
         })
     }
     
