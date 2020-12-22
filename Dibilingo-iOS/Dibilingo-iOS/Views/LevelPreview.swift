4198282a7fad5fa2dae2ac0623edb1cf3c5b8d3a
AginS <agins.main@gmail.com>
2020-12-23 01:08:34 +0300

    update level preview

--- Dibilingo-iOS/Dibilingo-iOS/Views/LevelPreview.swift
+++ Dibilingo-iOS/Dibilingo-iOS/Views/LevelPreview.swift
@@ -9,17 +9,92 @@ import SwiftUI
 
 struct LevelPreview: View {
     
+    let uiscreen = UIScreen.main.bounds
+    
     @ObservedObject var userprofile: UserProfile_ViewModel
     var category_name: String
     
     //@State private var showPopup: Bool = true
     
     var body: some View {
-        VStack(spacing: -5) {
+        VStack(spacing: 15) {
             Image("icon_\(category_name)")
                 .resizable()
                 .shadow(radius: 5)
-                .frame(width: 200, height: 200, alignment: .center)
+                .frame(width: 250, height: 250, alignment: .center)
+            
+            VStack(spacing: -10) {
+                ZStack {
+                RoundedRectangle(cornerRadius: 15)
+                    .shadow(radius: 10)
+                    .frame(width: uiscreen.width - 50, height: 75, alignment: .center)
+                    .padding()
+                    .foregroundColor(.white)
+                
+                    HStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width: 75, height: 75, alignment: .center)
+                            .foregroundColor(Color(.sRGB, red: 255/255, green: 203/255, blue: 204/255, opacity: 1.0))
+                            .padding(.leading, 25)
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        Spacer()
+                            .frame(width: 25)
+                        Text("Слова")
+                            .font(Font.custom("boomboom", size: 32))
+                    }
+                }
+                
+                ZStack {
+                RoundedRectangle(cornerRadius: 15)
+                    .shadow(radius: 10)
+                    .frame(width: uiscreen.width - 50, height: 75, alignment: .center)
+                    .padding()
+                    .foregroundColor(.white)
+                
+                    HStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width: 75, height: 75, alignment: .center)
+                            .foregroundColor(Color(.sRGB, red: 211/255, green: 233/255, blue: 255/255, opacity: 1.0))
+                            .padding(.leading, 25)
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        Spacer()
+                            .frame(width: 25)
+                        Text("Неправильные\nглаголы")
+                            .multilineTextAlignment(.center)
+                            .font(Font.custom("boomboom", size: 32))
+                    }
+                }
+                
+                ZStack {
+                RoundedRectangle(cornerRadius: 15)
+                    .shadow(radius: 10)
+                    .frame(width: uiscreen.width - 50, height: 75, alignment: .center)
+                    .padding()
+                    .foregroundColor(.white)
+                
+                    HStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width: 75, height: 75, alignment: .center)
+                            .foregroundColor(Color(.sRGB, red: 255/255, green: 229/255, blue: 157/255, opacity: 1.0))
+                            .padding(.leading, 25)
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        Spacer()
+                            .frame(width: 25)
+                        Text("Предложения")
+                            .font(Font.custom("boomboom", size: 32))
+                    }
+                }
+            }
+            /*
                 ZStack {
                     RoundedRectangle(cornerRadius: 50)
                         .frame(width: 275, height: 150, alignment: .center)
@@ -68,8 +143,8 @@ struct LevelPreview: View {
                             })
                         //
                         
-                    }
-                }
+                    } */
+                //}
         }
     }
 }
