3936249c5c4ded1ea6231e58fc2664fdb6815dc3
AginS <agins.main@gmail.com>
2020-12-23 17:11:19 +0300

    update serverurl

--- Dibilingo-iOS/Dibilingo-iOS/Views/LoginView.swift
+++ Dibilingo-iOS/Dibilingo-iOS/Views/LoginView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 #if targetEnvironment(simulator)
 let serverURL = "http://127.0.0.1:5000"
 #else
-let serverURL = "http://192.168.88.32:5000"
+//let serverURL = "http://192.168.88.32:5000"
+let serverURL = "http://192.168.43.82:5000"
 #endif
 
 struct LoginView: View {
